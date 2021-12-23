8f166f531371776a36d5fd0d3852cbf2499f129f
benzispawn <32866452+benzispawn@users.noreply.github.com>
2021-12-23 14:27:37 -0300

    Adding a controller

---
+++ WishList/Views/Home.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views
+{
+    public class Home : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
