6db8367d21e4367e7545f6a9ba4d65804380104b
benzispawn <32866452+benzispawn@users.noreply.github.com>
2021-12-24 15:30:20 -0300

    Completed module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -31,7 +31,7 @@ namespace WishList
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
             
